b0a62eea207d94e700b815d767458608939e4b22
IanSu18 <144109031+IanSu18@users.noreply.github.com>
2024-10-04 08:14:17 +0000

    gives answer to the weekend assignmet

---
+++ Ian_Sprint_Projectv2.75/ian_Sprint_Projectv2.76.py
@@ -0,0 +1,3 @@
+def list_factor(n):
+    return [i for i in range (1, n+1) if n % i == 0 ]
+    #This line of code list out every number that n are divisible by the number i. Which i is every number from 1 to n. 
